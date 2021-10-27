6062553b40e9912c5153b580ffed76aaf7632f85
TARTANGALH\2dam <penemax572@gmail.com>
2021-10-27 09:17:55 +0200

    Factorias hechas

---
+++ src/server/factory/LogicableFactory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server.factory;
+
+import lib.interfaces.Logicable;
+
+/**
+ *
+ * @author UnaiUrti
+ */
+public class LogicableFactory {
+    
+    /**
+     * Metodo de la factoria de la parte del servidor para implementar la clase 
+     * DAOableImplementation
+     * @return Devuelve un objeto de la interfaz Logicable el cual va a servir
+     * para luego implementar la siguiente clase
+     */
+    
+    public Logicable getDao(){
+        
+        Logicable dao = new DAOableImplementation();
+        
+        return dao;
+    }
+    
+}
