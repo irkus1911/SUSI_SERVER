092ca297eae6f5096f42ccf24db1a27a5d97004f
irkus1911 <83645063+irkus1911@users.noreply.github.com>
2021-11-02 08:49:24 +0100

    Dao para hacer merge

--- src/server/Server.java
+++ src/server/Server.java
@@ -5,22 +5,6 @@
  */
 package server;
 
-import java.sql.Timestamp;
-import java.time.Instant;
-import static java.time.Instant.now;
-import java.util.Scanner;
-import lib.dataModel.User;
-import lib.dataModel.UserPrivilege;
-import lib.dataModel.UserStatus;
-import lib.exceptions.ConnectException;
-import lib.exceptions.IncorrectEmailException;
-import lib.exceptions.IncorrectPasswordException;
-import lib.exceptions.IncorrectUserException;
-import lib.exceptions.PasswordDontMatchException;
-import lib.exceptions.UserDontExistException;
-import lib.exceptions.UserExistException;
-import server.controller.DAOableImplementation;
-
 /**
  *
  * @author 2dam
@@ -30,43 +14,7 @@ public class Server {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IncorrectUserException, IncorrectPasswordException, UserDontExistException, PasswordDontMatchException, ConnectException,IncorrectUserException, IncorrectPasswordException, IncorrectEmailException, UserExistException, PasswordDontMatchException, ConnectException {
-        Scanner sc =new Scanner(System.in);
-        User usu=null;
-        int decision;
-        System.out.println("Selecciona una opcion");
-       decision=sc.nextInt();
-                DAOableImplementation im=new DAOableImplementation();
-        switch(decision){
-            case 1:
-                usu=new User();
-                System.out.println("usuario");
-                usu.setLogin(sc.next());
-                System.out.println("email");
-                usu.setEmail(sc.next());
-                System.out.println("nombre completo");
-                usu.setFullName(sc.next());
-                usu.setStatus(UserStatus.ENABLED);
-                usu.setPrivilege(UserPrivilege.USER);
-                System.out.println("contraseña");
-                usu.setPassword(sc.next());
-                Instant instant = Instant.now();
-                Timestamp timestamp = Timestamp.from(instant);
-                usu.setLastPasswordChange(timestamp);
-                im.signUp(usu);
-                break;
-            case 2:
-                usu=new User();
-                System.out.println("usuario");
-                usu.setLogin(sc.next());
-                System.out.println("contraseña");
-                usu.setPassword(sc.next());
-                im.signIn(usu);
-                System.out.println("Hola"+usu.getLogin());
-                break;
-        }
+    public static void main(String[] args) {
         
-        // TODO code application logic here
     }
-    
 }
